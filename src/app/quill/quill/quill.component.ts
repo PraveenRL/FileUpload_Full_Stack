a3615f48333023ea14b9e387ccde07b437bf6389
PraveenRL <rlppraveen@gmail.com>
2019-12-25 18:27:16 +0530

    quill

--- src/app/quill/quill/quill.component.ts
+++ src/app/quill/quill/quill.component.ts
@@ -10,11 +10,16 @@ export class QuillComponent implements OnInit {
 
   editorForm: FormGroup;
   constructor() { }
-
+  
   ngOnInit() {
     this.editorForm = new FormGroup({
       'editor': new FormControl(null)
     })
+  }
+
+  onSubmit(){
+    console.log(this.editorForm.get('editor').value);
+    
   }
 
 }
